bb7414c30e4cff7c6c5d0731ded4221f7a9e074e
dmuey <dmuey@cpan.org>
2014-09-20 21:00:43 -0500

    Create ihaz.sh

---
+++ ihaz.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function ohai () {
+    echo -e "\033[1;34m➜➜➜➜\033[0m $1" 
+}
+
+function meep () {
+    echo -e "\033[1;33m⚠︎⚠︎⚠︎⚠︎\033[0m $1" 
+}
+
+function kbye () {
+    echo -e "\033[1;34m✓✓✓✓\033[0m $1" 
+}
